bb48640e9ef604816750eece30ad8a44a094306b
omki <aagaardmikkeli@gmail.com>
2018-11-05 22:20:59 +0100

    star menu

--- src/app/components/holomaster/holomaster.component.ts
+++ src/app/components/holomaster/holomaster.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoinSettings, SideSettings } from './coin.model';
 import { Mod } from '../models/design.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-holomaster',
@@ -8,6 +9,15 @@ import { Mod } from '../models/design.model';
   styleUrls: ['./holomaster.component.css']
 })
 export class HolomasterComponent implements OnInit {
+
+  title: string = "HolopediaLi";
+  description: string;
+  defaultDescription: string = `HoloMedicine
+  <br> HoloArt HoloApplications HoloAlgorithm
+  <br> HoloSmart HoloSustainability HoloSystems HoloSimulations
+  <br> HoloTechnology HoloTherapies HoloTunnels Holotomography
+  <br> HoloEducation HoloEmulation
+  <br> HoloResearch`
 
   coins: CoinSettings[] = [
     new CoinSettings(["Research"], { theme:  "purple" }, [], { url: this.getFctcoinUrl(".2") }),
@@ -24,12 +34,42 @@ export class HolomasterComponent implements OnInit {
   ]
   selectedMod = this.mods[0];
 
+  menuItems = [
+    {name: "HolopediaLi", style: { top: "20px", left: "50%", transform: "translateX(-50%)"}},
+    {name: "Medicine", style: { top: "64px", left: "calc(50% + 97px)"}},
+    {name: "Art", style: { top: "123px", left: "calc(50% + 190px)"}},
+    {name: "Applications", style: { top: "216px", left: "calc(50% + 246px)"}},
+    {name: "Algorithms", style: { top: "320px", left: "calc(50% + 266px)"}},
+    {name: "Smart", style: { top: "424px", left: "calc(50% + 246px)"}},
+    {name: "Sustainability", style: { top: "516px", left: "calc(50% + 190px)"}},
+    {name: "Systems", style: { top: "574px", left: "calc(50% + 97px)"}},
+    {name: "Simulations", style: { top: "630px", left: "50%", transform: "translateX(-50%)"}},
+    {name: "Technology", style: { top: "574px", left: "calc(50% - 97px)", transform: "translateX(-100%)"}},
+    {name: "Therapies", style: { top: "516px", left: "calc(50% - 190px)", transform: "translateX(-100%)"}},
+    {name: "Tunnels", style: { top: "424px", left: "calc(50% - 246px)", transform: "translateX(-100%)"}},
+    {name: "Tomography", style: { top: "320px", left: "calc(50% - 266px)", transform: "translateX(-100%)"}},
+    {name: "Education", style: { top: "216px", left: "calc(50% - 246px)", transform: "translateX(-100%)"}},
+    {name: "Emulation", style: { top: "123px", left: "calc(50% - 190px)", transform: "translateX(-100%)"}},
+    {name: "Research", style: { top: "64px", left: "calc(50% - 97px)", transform: "translateX(-100%)"}},
+  ]
+
+  
   starBgUrl: string;
   starIndex: number;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { 
+  }
 
   ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.title = params.holo;
+      this.description = (params.holo == "HolopediaLi")? this.defaultDescription: "";
+    });
+
+    
     let vid = <HTMLMediaElement>document.getElementById("quantumneurofeedback");
     vid.volume = 0.1;
     setInterval(x => this.iterateStarBg(), 810);
